6f7dc648fae7e57a8d11c280e7284f5df219f31a
Guilherme Csorgo Henriques <guilherme.henriques@andritz.com>
2019-08-16 17:30:39 -0300

    Added favored stuff

---
+++ Finances.App/Finances.App/ViewModels/FavoredViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Finances.App.ViewModels
+{
+    public class FavoredViewModel : BaseViewModel
+    {
+        #region Commands
+
+        public ICommand NewFavoredCommand { get; }
+
+        #endregion
+    }
+}
--- Finances.App/Finances.App/Views/Favoreds.xaml.cs
+++ Finances.App/Finances.App/Views/Favoreds.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Finances.App.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,12 @@ namespace Finances.App.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Favoreds : ContentPage
     {
+        FavoredViewModel viewModel; 
         public Favoreds()
         {
             InitializeComponent();
+
+            BindingContext = viewModel = new FavoredViewModel();
         }
     }
 }

820301833d638cda2cc0e72f17589b251c9a57c3
Guilherme Csorgo <csorgodev@hotmail.com>
2019-08-17 13:53:40 -0300

    Some favored stuff

---
+++ Finances.App/Finances.App/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace Finances.App.Models
+{
+    public class Account
+    {
+        public int? Bank { get; set; }
+        public int? BankBranch { get; set; }
+        public int? BankAccount { get; set; }
+        public int? BankAccountDigit { get; set; }
+    }
+}
--- Finances.App/Finances.App/ViewModels/FavoredViewModel.cs
+++ Finances.App/Finances.App/ViewModels/FavoredViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Finances.App.Models;
+using Finances.App.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Finances.App.ViewModels
 {
@@ -12,5 +16,73 @@ namespace Finances.App.ViewModels
         public ICommand NewFavoredCommand { get; }
 
         #endregion
+
+        #region Properties
+
+        public IEnumerable<Favored> Favoreds { get; private set; }
+
+        #endregion
+
+        public FavoredViewModel()
+        {
+            NewFavoredCommand = new Command(async _ => await NewFavored());
+
+            //Favoreds = new FavoredService().GetFavoredByUser();
+            Favoreds = new List<Favored>
+            {
+                new Favored
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Adebaior da Silva",
+                    TaxNumber = "427.174.187-15",
+                    BelongToUserId = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now.AddDays(-3),
+                    Account = new Account
+                    {
+                        Bank = 123,
+                        BankAccount = 12345,
+                        BankBranch = 2344,
+                        BankAccountDigit = 1
+                    }
+                },
+
+                new Favored
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Adebaior da Silva",
+                    TaxNumber = "427.174.187-15",
+                    BelongToUserId = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now.AddDays(-3),
+                    Account = new Account
+                    {
+                        Bank = 123,
+                        BankAccount = 12345,
+                        BankBranch = 2344,
+                        BankAccountDigit = 1
+                    }
+                },
+
+                new Favored
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Adebaior da Silva",
+                    TaxNumber = "427.174.187-15",
+                    BelongToUserId = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now.AddDays(-3),
+                    Account = new Account
+                    {
+                        Bank = 123,
+                        BankAccount = 12345,
+                        BankBranch = 2344,
+                        BankAccountDigit = 1
+                    }
+                }
+            };
+        }
+
+        public async Task NewFavored()
+        {
+            
+        }
     }
 }

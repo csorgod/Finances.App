48f2d5952e525733d34fc69c7a96cd7d79f5be75
Guilherme Csorgo <csorgodev@hotmail.com>
2019-09-02 21:43:32 -0300

    Fab working with rotation animation

---
+++ Finances.App/Finances.App/Components/FloatingActionButton.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Finances.App.Helpers.Animation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Finances.App.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FloatingActionButton : ContentView
+    {
+        bool IsActive { get; set; } = false;
+
+        public FloatingActionButton()
+        {
+            InitializeComponent();
+        }
+
+        private async void FABPrimary_Clicked(object sender, EventArgs e)
+        {
+            if(!IsActive)
+                await Animator.FAB_Rotate90Degrees(sender as Button);
+            else 
+                await Animator.FAB_RotateBack90Degrees(sender as Button);
+
+            IsActive = !IsActive;
+        }
+    }
+}
---
+++ Finances.App/Finances.App/Helpers/Animation/Animator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Finances.App.Helpers.Animation
+{
+    public static class Animator
+    {
+        public async static Task FAB_Rotate90Degrees(Button fab)
+        {
+            await fab.RotateTo(315, easing: Easing.Linear);
+        }
+
+        public async static Task FAB_RotateBack90Degrees(Button fab)
+        {
+            await fab.RotateTo(0, easing: Easing.Linear);
+        }
+    }
+}
